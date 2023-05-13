364e77d7ee94f63dd8d8ed6ec186a6531b0147d0
Tevon Davis <tevondavisjc@gmail.com>
2023-05-12 21:14:23 -0500

    use sale and user model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Sale;
+use App\Models\User;
 
 class Customer extends Model
 {
