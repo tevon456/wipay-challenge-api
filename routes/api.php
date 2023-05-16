5b91b4282be67270bce9b2bff4303bee7d799868
Tevon Davis <tevondavisjc@gmail.com>
2023-05-15 22:33:09 -0500

    wip

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'books'], function () {
+    Route::get('/', function () {
+        return [
+            'hello' => 'world'
+        ];
+    });
+});
