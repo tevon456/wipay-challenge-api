0c73937dc2f5cc7f4efdd80abeb72001537b7acf
Tevon Davis <tevondavisjc@gmail.com>
2023-05-16 12:19:26 -0500

    create book controller methods

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,24 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $user = auth()->user();
+        $perPage = $request->query('per_page', 10);
+        $books = $user?->role == 'admin' ? Book::with('sales')->withCount('sales')->paginate($perPage) : Book::paginate($perPage);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json($books);
     }
 
     /**
@@ -27,7 +25,22 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validation = Validator::make($request->all(), [
+            'author_name' => 'required',
+            'title' => 'required',
+            'isbn' => 'required|unique:books,isbn',
+            'price' => 'required|numeric|min:0',
+            'inventory_count' => 'required|integer',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json(['errors' => $validation->errors()], 400);
+        }
+
+        $book = Book::create($request->all());
+
+        return response()->json($book, 201);
     }
 
     /**
@@ -35,7 +48,16 @@ class BookController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = auth()->user();
+        $book = $user?->role == 'admin' ? Book::with('sales')->withCount('sales')->find($id) : Book::find($id);
+
+        if (!$book) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        return response()->json($book);
     }
 
     /**
@@ -43,7 +65,31 @@ class BookController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
+        $validation = Validator::make($request->all(), [
+            'author_name' => 'required',
+            'title' => 'required',
+            'isbn' => 'required|unique:books,isbn',
+            'price' => 'required|numeric|min:0',
+            'inventory_count' => 'required|integer',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json(['errors' => $validation->errors()], 400);
+        }
+
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        // Update the book attributes with the validated data
+        $book->update($request->all());
+
+        return response()->json($book, 200);
     }
 
     /**
@@ -51,6 +97,18 @@ class BookController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        $book->delete();
+
+        return response()->json([
+            'message' => 'book deleted successfully',
+        ]);
     }
 }
