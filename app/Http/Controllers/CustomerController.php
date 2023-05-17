6b7c4e2b7d61f71ef745b6fe6f3f4ca0878a21ba
Tevon Davis <tevondavisjc@gmail.com>
2023-05-16 23:55:05 -0500

    customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,32 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $perPage = $request->query('per_page', 10);
+        $customers = Customer::with(['sales' => 'purchases'], 'user')->withCount(['sales' => 'purchases_count'])->paginate($perPage);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json($customers);
     }
 
     /**
@@ -35,7 +25,23 @@ class CustomerController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = auth()->user();
+
+        if ($user?->role != 'admin' || $user?->id != $id) {
+            return response()->json(['errors' => [
+                'message' => 'action unauthorized'
+            ]], 403);
+        }
+
+        $customer = Customer::with(['sales' => 'purchases'], 'user')->withCount(['sales' => 'purchases_count'])->find($id);
+
+        if (!$customer) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        return response()->json($customer);
     }
 
     /**
@@ -43,7 +49,51 @@ class CustomerController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validation = Validator::make($request->all(), [
+            'phone_number' => 'required|phone',
+            'address_line_1' => 'required',
+            'address_line_2' => 'nullable',
+            'city' => 'required',
+            'parish' => 'required',
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json(['errors' => $validation->errors()], 400);
+        }
+
+        $user = auth()->user();
+
+        if ($user?->role != 'admin' || $user?->id != $id) {
+            return response()->json(['errors' => [
+                'message' => 'action unauthorized'
+            ]], 403);
+        }
+
+        $customer = Customer::with(['sales' => 'purchases'], 'user')->withCount(['sales' => 'purchases_count'])->find($id);
+
+        if (!$customer) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        $user = User::find($customer?->user_id);
+        $user->update([
+            'name' => $request->input('name'),
+            'email' => $request->input('email')
+        ]);
+
+        $customer->update([
+            'phone_number' => $request->input('phone_number'),
+            'address_line_1' => $request->input('address_line_1'),
+            'address_line_2' => $request->input('address_line_2'),
+            'city' => $request->input('city'),
+            'parish' => $request->input('parish'),
+        ]);
+
+        return response()->json($customer);
     }
 
     /**
@@ -51,6 +101,21 @@ class CustomerController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return response()->json(['errors' => [
+                'message' => 'requested resource not found'
+            ]], 404);
+        }
+
+        $user = User::find($customer?->user_id);
+
+        // will cascade to customer
+        $user->delete();
+
+        return response()->json([
+            'message' => 'customer deleted successfully',
+        ]);
     }
 }
